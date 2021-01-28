6f09a6c4fc33510e8c5bb103b3d2e660d4bbeb1d
T-Mook <msp770@gmail.com>
2021-01-28 20:18:20 +0900

    [INFRA] gh-pages 사용 위한 기본 설치

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/T-Mook.github.io/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
