725c69979d48206749b4f682c870b323336b1b8f
T-Mook <msp770@gmail.com>
2021-01-29 15:48:43 +0900

    [FEATURE] Carreer & indev 배경 및 카드 이미지 삽입 기초

--- components/index/Carreer.vue
+++ components/index/Carreer.vue
@@ -1,17 +1,38 @@
 <template>
-  <v-timeline>
+  <v-timeline :dense="$vuetify.breakpoint.smAndDown" class="mx-4">
     <v-timeline-item
       v-for="(item, index) in items"
       :key="index"
       :color="item.color"
+      :left="item.left"
+      :right="!item.left"
+      :hide-dot="item.hideDot"
+      small
     >
       <template #opposite>
         <span>{{ item.time }}</span>
       </template>
-      <v-card class="elevation-2">
+      <v-card outlined flat>
+        <v-img
+          v-if="!!item.src"
+          :src="item.src"
+          max-width="640"
+          contain
+        ></v-img>
+        <template #placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
         <v-card-title class="headline">{{ item.title }}</v-card-title>
-        <v-card-text>
+        <v-card-text v-if="item.content.length !== 0">
           {{ item.content }}
+        </v-card-text>
+        <v-card-text v-if="$vuetify.breakpoint.smAndDown" class="mt-0 pt-0">
+          <small>{{ item.time }}</small>
         </v-card-text>
       </v-card>
     </v-timeline-item>
@@ -23,12 +44,109 @@ export default {
   data() {
     return {
       items: [
-        { title: '제목', time: '내용', content: '내용', color: 'blue' },
-        { title: '제목', time: '내용', content: '내용', color: 'blue' },
-        { title: '제목', time: '내용', content: '내용', color: 'blue' },
-        { title: '제목', time: '내용', content: '내용', color: 'blue' },
+        // {
+        //   title: '「EASYTREND」서비스 개발',
+        //   time: '2020.7',
+        //   content:
+        //     '사이드 프로젝트로 사람들이 특정 검색어에 대한 정보를 쉽게 얻고 저장할 수 있게 만드는 서비스 개발',
+        //   color: 'blue lighten-1',
+        // },
+        {
+          title: '「쉬운지식 유한책임회사」 설립',
+          time: '2020.7',
+          content: '지식의 저주를 해소하는 서비스들의 집합을 위한 회사 설립',
+          color: 'red lighten-1',
+          left: true,
+          hideDot: false,
+        },
+        {
+          title: '「EASYXPLAIN」 웹서비스 개발(중)',
+          time: '2019.7',
+          content: '쉬운 지식을 모으는 웹서비스 직접 개발 시작',
+          color: 'blue lighten-2',
+          src: '/carreer/easyxplain_og.png',
+          left: false,
+          hideDot: false,
+        },
+        {
+          title: '『디지털 인재의 8가지 기본지식』 강의',
+          time: '2019.8',
+          content: '현대C&R 주관, 내용전문가 참여 및 출연',
+          color: 'blue lighten-1',
+          src: '/carreer/digital8.png',
+          left: true,
+          hideDot: false,
+        },
+        {
+          title: '「Deep Learning」 추천 영상 Youtube 1위',
+          time: '2019.2',
+          content: '본 시점 기준 약 27,000 view',
+          color: 'blue lighten-2',
+          left: false,
+          hideDot: true,
+        },
+        {
+          title: '교보문고 북모닝 - All that BlockChain',
+          time: '2019.2',
+          content: '블록체인의 모든 것 출연 및 진행',
+          color: 'blue lighten-2',
+          src: '/carreer/2019-02-13_15;45;26.png',
+          left: true,
+          hideDot: false,
+        },
+        {
+          title: '『보면 아는 블록체인』, 정보문화사 발간',
+          time: '2018.7',
+          content:
+            '블록체인에 대해 쉽게 설명하는 내용으로 책 집필 및 발간 (photo by Doori Kim)',
+          color: 'red lighten-1',
+          src: '/carreer/mybook_doori_20181102_025716376.jpg',
+          left: true,
+          hideDot: false,
+        },
+        {
+          title: '「Block Chain」 추천 영상 Youtube 1위',
+          time: '2018.1',
+          content: '본 시점 기준 약 350,000 view',
+          color: 'blue lighten-2',
+          left: false,
+          hideDot: true,
+        },
+        {
+          title: 'Channel『TMook』',
+          time: '2016.4',
+          content: '어려운 용어에 대한 쉬운 설명을 제공하는 채널 운영 시작',
+          color: 'red lighten-1',
+          src: '/carreer/2021-01-29 135905.png',
+          left: true,
+          hideDot: false,
+        },
+        {
+          title: '매일경제TV M머니 출연',
+          time: '2015.3 - 2015.10',
+          content: '해외직접투자하기 & 글로벌 이슈 5 PB 패널 출연',
+          color: 'blue lighten-2',
+          left: true,
+          hideDot: false,
+        },
+        {
+          title: '(주)유안타증권',
+          time: '2014.7 - 2017.7',
+          content: 'PB(지점영업), Retail전략팀 근무',
+          color: 'red lighten-1',
+          src: '/carreer/agreement-3489902_1920.jpg',
+          left: true,
+          hideDot: false,
+        },
       ],
     }
   },
 }
 </script>
+
+<style scoped>
+.v-card__title.headline {
+  font-size: 1rem !important;
+  font-weight: 600;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,73 @@
 <template>
   <v-row justify="center" align="center">
-    <v-card :max-width="mainCardMaxWidth" flat>
-      <v-row>
-        <v-col cols="12">
-          <v-card outlined flat>
-            <v-card-title class="headline"> TMook's Page </v-card-title>
-            <v-card-text> 박성묵(TMook)의 개인 페이지입니다 </v-card-text>
-            <v-card-actions>
-              <v-spacer />
-              <v-spacer />
-              <v-btn
-                v-for="(linkBtn, index) in externalLinkBtn"
-                :key="index"
-                :href="linkBtn.url"
-                :color="linkBtn.color"
-                outlined
-                small
-                >{{ linkBtn.name }}</v-btn
-              >
-            </v-card-actions>
-          </v-card>
+    <!-- ### Start: LinkedIn Embeding Code ### -->
+    <script
+      type="text/javascript"
+      src="https://platform.linkedin.com/badges/js/profile.js"
+      async
+      defer
+    ></script>
+    <v-card :max-width="mainCardMaxWidth" flat color="rgba(0,0,0,0)">
+      <v-row class="mt-0">
+        <v-col cols="12" class="py-0">
+          <v-img src="/background/stock-1863880_1920.jpg" class="align-center">
+            <v-card-title class="justify-center white--text"
+              >Read The World, Lead The World</v-card-title
+            >
+          </v-img>
         </v-col>
-        <v-col cols="12">
+      </v-row>
+      <!-- LinkedIn Badge -->
+      <!--
+      <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="ko_KR" data-type="vertical" data-theme="dark" data-vanity="성묵-박-34a0a2135"><a class="LI-simple-link" href=\'https://kr.linkedin.com/in/%EC%84%B1%EB%AC%B5-%EB%B0%95-34a0a2135?trk=profile-badge\'>박성묵</a></div>'
+      -->
+      <!-- 
+      <v-row class="mt-0">
+        <v-col cols="12" class="py-0">
+          <v-img src="/background/stock-1863880_1920.jpg" class="align-end">
+            <v-card-title class="white--text"
+              >Experience in Finance</v-card-title
+            >
+          </v-img>
+        </v-col>
+      </v-row>
+      -->
+      <!--
+      <v-row class="mt-0">
+        <v-col cols="12" class="py-0">
+          <v-img src="/background/code-1839406_1920.jpg" class="align-end">
+            <v-card-title class="white--text"
+              >Experience in Programming</v-card-title
+            >
+          </v-img>
+        </v-col>
+      </v-row>
+      <v-row class="mt-0">
+        <v-col cols="12" class="py-0">
+          <v-img src="/background/business-2884023_1920.jpg" class="align-end">
+            <v-card-title class="white--text"
+              >Experience in Strategy Planning Team</v-card-title
+            >
+          </v-img>
+        </v-col>
+      </v-row>
+      -->
+      <v-row
+        :justify="$vuetify.breakpoint.smAndDown ? 'start' : 'center'"
+        class="mt-7 mx-5"
+      >
+        <v-btn
+          v-for="(linkBtn, index) in externalLinkBtn"
+          :key="index"
+          :href="linkBtn.url"
+          :color="linkBtn.color"
+          small
+          icon
+          ><v-icon>{{ linkBtn.name }}</v-icon></v-btn
+        >
+      </v-row>
+      <v-row>
+        <v-col v-if="false" cols="12">
           <skills />
         </v-col>
         <v-col cols="12">
@@ -45,9 +91,15 @@ export default {
     return {
       externalLinkBtn: [
         {
-          name: 'Youtube',
+          name: 'mdi-youtube',
           url: 'https://www.youtube.com/c/TMook',
           color: 'red darken-1',
+        },
+        {
+          name: 'mdi-instagram',
+          url:
+            'https://www.youtube.com/redirect?event=channel_banner&redir_token=QUFFLUhqbGJ2dWZ2TWtWUVpXN1FHZEU5VW5pMDdyODN3Z3xBQ3Jtc0trdXpMaVAtVUwtaUNKbUo1THZ5OVRSRkFQUE9pN1JWb29wV3JnRl9DSkQtclRyM3I3X2Z5dVZ3N2drLXlyWjZmSGljUklkUjNXcmdiV0ZwLUttMTM2MURJTTVYdjZ2UlJWZVV3akI5WTVGZzFQWjA3MA&q=https%3A%2F%2Fwww.instagram.com%2Fchannel_tmook%2F',
+          color: 'pink lighten-3',
         },
       ],
     }
@@ -55,9 +107,9 @@ export default {
   computed: {
     mainCardMaxWidth() {
       const maxWidth =
-        this.$vuetify.breakpoint.width < 800
+        this.$vuetify.breakpoint.width < 1280
           ? this.$vuetify.breakpoint.width
-          : 800
+          : 1280
       return maxWidth
     },
   },
