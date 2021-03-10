93e17e9c105b9baa5776972227121ef533697bbb
T-Mook <msp770@gmail.com>
2021-03-10 11:51:33 +0900

    [INFRA] MainFooter.vue ts 적용

--- components/default/MainFooter.vue
+++ components/default/MainFooter.vue
@@ -1,9 +1,21 @@
 <template>
-  <v-footer color="rgba(21,31,32,1)" padless>
+  <v-footer :color="color" padless>
     <v-row justify="center" no-gutters>
       <v-col class="py-4 text-center grey--text" cols="12">
-        contact : msp770@gmail.com
+        {{ contact }}
       </v-col>
     </v-row>
   </v-footer>
 </template>
+
+<script lang="ts">
+import { Component, Provide, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+class ComponentsDefaultMainFooter extends Vue {
+  @Provide() contact: string = 'contact : msp770@gmail.com'
+  @Provide() color: string = 'rgba(21,31,32,1)'
+}
+
+export default ComponentsDefaultMainFooter
+</script>
