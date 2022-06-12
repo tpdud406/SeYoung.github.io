87ba3d33373944ccb1e0287c167dd96d8c827b58
T-Mook <msp770@gmail.com>
2022-06-12 18:27:57 +0900

    [#7][WIP][FEATURE] Projects, Best Youtube 추가

--- components/index/MainProject.vue
+++ components/index/MainProject.vue
@@ -1,66 +1,45 @@
 <template>
-  <v-card flat tile>
-    <!-- ### Start : title name ### -->
-    <v-card-title class="d-flex justify-center">
-      <p
-        class="text-center text-h3 text-sm-h2 font-weight-black"
-        v-html="title"
-      />
-    </v-card-title>
-
-    <v-card-subtitle class="text-center">
-      <p class="text-center" v-html="desc" />
-    </v-card-subtitle>
-
-    <!-- Start : Contents -->
-    <v-container fluid>
-      <v-row no-gutters>
-        <v-col
-          v-for="(project, index) in projects"
-          :key="index"
-          cols="12"
-          md="6"
+  <v-row justify="center">
+    <v-col v-for="(project, index) in projects" :key="index" cols="12" md="5">
+      <v-card :width="width" flat tile dark>
+        <v-img
+          :src="project.src"
+          :gradient="project.color.backgroundGradient"
+          :height="heightInXS"
+          :max-width="
+            $vuetify.breakpoint.mdAndUp
+              ? $vuetify.breakpoint.width / 2
+              : $vuetify.breakpoint.width
+          "
+          class="align-center"
         >
-          <v-card :width="width" flat tile>
-            <v-img
-              :src="project.src"
-              :gradient="project.color.backgroundGradient"
-              :height="heightInXS"
-              :max-width="
-                $vuetify.breakpoint.mdAndUp
-                  ? $vuetify.breakpoint.width / 2
-                  : $vuetify.breakpoint.width
-              "
-              class="align-center"
-            >
-              <v-card-subtitle class="project-subtitle">
-                {{ project.text.subtitle }}
-              </v-card-subtitle>
+          <v-card-subtitle class="text-md-h6 grey--text text--lighten-2 pb-0">
+            {{ project.text.subtitle }}
+          </v-card-subtitle>
 
-              <v-card-title class="project-title">
-                {{ project.text.title }}
-              </v-card-title>
+          <v-card-title class="text-md-h4 white--text">
+            {{ project.text.title }}
+          </v-card-title>
 
-              <v-card-text
-                class="project-content white--text"
-                v-html="project.text.content"
-              />
+          <v-card-text
+            class="text-md-body-2 grey--text text--lighten-2"
+            v-html="project.text.content"
+          />
 
-              <v-btn
-                outlined
-                depressed
-                :color="project.btn.color"
-                :href="project.btn.href"
-                class="mt-6 ml-5"
-              >
-                {{ project.btn.name }}
-              </v-btn>
-            </v-img>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+          <v-btn
+            outlined
+            rounded="pill"
+            depressed
+            :color="project.btn.color"
+            :href="project.btn.href"
+            class="mt-6 ml-5"
+          >
+            {{ project.btn.name }}
+          </v-btn>
+        </v-img>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -105,16 +84,16 @@ class ComponentsIndexMainProject extends Vue {
         title: '쉬운 설명을 제공하거나, 얻으세요',
         content:
           "사람들이 작성하는 용어 설명 및 가이드, 이곳에서 그 평가기준은 오직 '쉽다'는 것. " +
-          '<br>쉽게 설명하는 능력을 인정받는 공간이기도 합니다',
+          '쉽게 설명하는 능력을 인정받는 공간이기도 합니다',
       },
       color: {
-        backgroundGradient: 'to left, rgba(41, 121, 255,.7), rgba(30,36,77,1)',
+        backgroundGradient: 'to left, rgba(0, 0, 0,.7), rgba(0,0,0,1)',
       },
       src: '/background/office-1209640_1920.jpg',
       btn: {
         name: '바로가기 ▶',
         href: 'https://ko.meaniit.com/main',
-        color: 'blue lighten-4',
+        color: 'grey lighten-2',
       },
       cardClass: 'align-center',
     },
@@ -124,17 +103,16 @@ class ComponentsIndexMainProject extends Vue {
         title: '금융/경제/IT 설명 쉽게 듣고 보세요',
         content:
           '실시간 소통 또는 편집영상을 통해 관심있는, 또는 화제의 어려운 용어들을 다룹니다. ' +
-          '<br>매주 수요일 저녁 9시 LIVE 진행됩니다',
+          '매주 수요일 저녁 9시 LIVE 진행됩니다',
       },
       color: {
-        backgroundGradient:
-          'to left bottom, rgba(11,12,23,1), rgba(11,12,23,.6)',
+        backgroundGradient: 'to left bottom, rgba(0,0,0,1), rgba(0,0,0,.3)',
       },
       src: '/background/radio-2722271_1920.jpg',
       btn: {
         name: 'Youtube ▶',
         href: 'https://youtube.com/c/TMook',
-        color: 'red lighten-2',
+        color: 'grey lighten-2',
       },
       cardClass: 'justify-end align-center',
     },
@@ -143,26 +121,3 @@ class ComponentsIndexMainProject extends Vue {
 
 export default ComponentsIndexMainProject
 </script>
-
-<style scoped>
-.main-title {
-  font-size: 1.6rem !important;
-  font-weight: 500;
-}
-.project-subtitle {
-  font-size: 1.1rem !important;
-  font-weight: 400;
-  padding-bottom: 10px;
-  color: rgba(235, 235, 235, 1) !important;
-}
-.project-title {
-  font-size: 1.5rem !important;
-  padding-top: 0px;
-  font-weight: 500;
-  color: white !important;
-}
-.project-content {
-  font-size: 0.9rem !important;
-  font-weight: 300;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -35,6 +35,20 @@
       :class="classOfComponent + ' pt-md-10'"
     />
     <memories :class="classOfComponent" />
+
+    <!-- Start : Main Project -->
+    <sub-title
+      :parent-items="objectOfParentItems.mainProject"
+      :class="classOfComponent + ' pt-md-10'"
+    />
+    <main-project :class="classOfComponent" />
+
+    <!-- Start : Main Youtube Video -->
+    <sub-title
+      :parent-items="objectOfParentItems.mainYoutubeVideo"
+      :class="classOfComponent + ' pt-md-10'"
+    />
+    <main-youtube-video :class="classOfComponent" />
   </v-container>
 </template>
 
@@ -44,6 +58,8 @@ import TopIntroduce from '@/components/index/TopIntroduce.vue'
 import SubTitle from '@/components/index/SubTitle.vue'
 import Advantages from '@/components/index/Advantages.vue'
 import Memories from '@/components/index/Memories.vue'
+import MainProject from '@/components/index/MainProject.vue'
+import MainYoutubeVideo from '@/components/index/MainYoutubeVideo.vue'
 
 @Component({
   components: {
@@ -51,6 +67,8 @@ import Memories from '@/components/index/Memories.vue'
     SubTitle,
     Advantages,
     Memories,
+    MainProject,
+    MainYoutubeVideo,
   },
 })
 class PagesIndex extends Vue {
@@ -66,6 +84,8 @@ class PagesIndex extends Vue {
   private objectOfParentItems: {
     advantages: { title: string; content: string }
     memory: { title: string; content: string }
+    mainProject: { title: string; content: string }
+    mainYoutubeVideo: { title: string; content: string }
   } = {
     advantages: {
       title: 'Advantages',
@@ -74,7 +94,16 @@ class PagesIndex extends Vue {
     },
     memory: {
       title: 'Memories',
-      content: '경제&#183;금융&#183;IT 분야 주요 트렌드와 함께했던 기억',
+      content: '경제&#183;금융&#183;IT 분야 주요 트렌드와 함께했던 기억이에요',
+    },
+    mainProject: {
+      title: 'Main Project',
+      content: '최근 많은 분들과 함께 진행되고 있는 주요 프로젝트입니다',
+    },
+    mainYoutubeVideo: {
+      title: 'Best Youtube',
+      content:
+        '경제&#183;금융&#183;IT 분야에서 많은 분들이 시청해주신 영상들이에요',
     },
   }
 }
